a09e79c10ce2ad0cc1601d2d314b8cb2a2f70281
Almadny <Almadny2u@gmail.com>
2023-07-06 17:26:38 +0000

    Added an annotated function that concatenates two string and return the result

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Annotation Project Add Function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ A function that adds two numbers """
+
+    return f"{str1}{str2}"
