d77be4545ed6251f6c470d4c70e1819466e8290a
Almadny <Almadny2u@gmail.com>
2023-07-06 17:37:00 +0000

    Added a function that floors a given float to return an integer

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import math
+"""
+Annotation Project Add Function
+"""
+
+
+def floor(n: float) -> int:
+    """ A function that adds two numbers """
+
+    return math.floor(n)
