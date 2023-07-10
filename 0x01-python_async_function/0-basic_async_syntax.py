12a7e4ab9d2b4d12b1985ced7bb38efde5883703
Almadny <Almadny2u@gmail.com>
2023-07-10 17:17:19 +0000

    Basic Async function that delays for a specified time

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+My random wait module
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Coroutine that delays for a time and returns the delay time """
+    delay = random.uniform(0, max_delay + 1)
+    await asyncio.sleep(delay)
+    return delay
