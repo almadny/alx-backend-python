97e38f29dc3aa2982c97e9b3fc2501a440dd156e
Almadny <Almadny2u@gmail.com>
2023-07-11 17:00:54 +0000

    Added README and 0-async_generator module

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module Containing async_generator coroutine
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """
+    Asynchronous generator function that creates an iterator
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        randNum = random.uniform(0, 10)
+        yield randNum
